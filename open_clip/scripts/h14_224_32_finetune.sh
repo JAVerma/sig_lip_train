ce52859b18cac2f29579ff4d50f35f3ac275c3de
shubhamkalantri <shubhamkalantri123@gmail.com>
2024-06-11 08:53:53 +0000

    dfn

--- open_clip/scripts/h14_224_32_finetune.sh
+++ open_clip/scripts/h14_224_32_finetune.sh
@@ -3,7 +3,7 @@ TORCH_CUDNN_V8_API_ENABLED=1 torchrun --nproc_per_node 1 -m training.main \
     --save-frequency 1 \
     --save-most-recent \
     --zeroshot-frequency 1 \
-    --train-data 'jiviai/rsna_biomedclip_failures' \
+    --train-data 'jiviai/xray_caption_conv' \
     --dataset-type "hf" \
     --lr "2.048e-4" \
     --beta1 0.9 \
@@ -14,7 +14,7 @@ TORCH_CUDNN_V8_API_ENABLED=1 torchrun --nproc_per_node 1 -m training.main \
     --aug-cfg scale='(0.4, 1.0)' color_jitter='(0.32, 0.32, 0.32, 0.08)' color_jitter_prob=0.5 gray_scale_prob=0.5 \
     --epochs=10 \
     --workers=1 \
-    --model "hf-hub:microsoft/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224" \
+    --model "hf-hub:apple/DFN5B-CLIP-ViT-H-14-378" \
     --precision 'amp_bf16' \
     --local-loss \
     --gather-with-grad \
@@ -23,8 +23,8 @@ TORCH_CUDNN_V8_API_ENABLED=1 torchrun --nproc_per_node 1 -m training.main \
     --log-every-n-steps 32 \
     --seed 0 \
     --logsdir ./logs/ \
-    --name 'BioMedCLIP-ImageFail-ProjOnly' \
+    --name 'DFN-ImageFailLatest2' \
     --report-to "wandb" \
-    --wandb-project-name "BioMedCLIP-finetune-chest" \
+    --wandb-project-name "DFN-finetune-chest" \
     --use-last4
     # --val-data 'jiviai/xray_caption_conv' \
